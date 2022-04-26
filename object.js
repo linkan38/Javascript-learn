982495c51104549f27ab5744478dc9d45c7d8e19
linkan <llingkon@gmail.com>
2022-04-26 14:14:00 +0600

    lknkl

--- object.js
+++ object.js
@@ -4,3 +4,10 @@ console.log("abir er number is " +student.batch );
 student.id = 979879;
 console.log(uni);
 console.log(student.id);
+
+var student = { id: 1233, phone: 93479867, batch: 868698986 }
+var uni ="abir er new id " +student.batch;
+console.log("abir er number is " +student.batch );
+student.id = 9867986896;
+console.log(uni);
+console.log(student.id);
