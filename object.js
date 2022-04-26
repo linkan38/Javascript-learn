d99e7827da771a50cdbbc194b37a9fdb372c1138
linkan <llingkon@gmail.com>
2022-04-26 14:17:35 +0600

    kjhkj

--- object.js
+++ object.js
@@ -5,9 +5,3 @@ student.id = 979879;
 console.log(uni);
 console.log(student.id);
 
-var student = { id: 1233, phone: 93479867, batch: 868698986 }
-var uni ="abir er new id " +student.batch;
-console.log("abir er number is " +student.batch );
-student.id = 9867986896;
-console.log(uni);
-console.log(student.id);
